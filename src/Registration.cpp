f8bc111db646c7942bc2f00074f14e878fbd730d
Davide Sarraggiotto <sarra99@live.it>
2023-05-28 19:08:51 +0200

    main loop and svd

--- src/Registration.cpp
+++ src/Registration.cpp
@@ -95,6 +95,31 @@ void Registration::execute_icp_registration(double threshold, int max_iteration,
   //Remember to update transformation_ class variable, you can use source_for_icp_ to store transformed 3d points.
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  int iter_ = 0;
+  double prev_rmse_ = 0;
+
+  while (iter_++ < max_iteration) {
+    Eigen::Matrix4d cur_transformation;
+
+    std::vector<size_t> source_indices;
+    std::vector<size_t> target_indices;
+    double cur_rmse;
+
+    // C++11
+    tie(source_indices, target_indices, cur_rmse) = Registration::find_closest_point(threshold);
+
+    if (mode == "svd") {
+      cur_transformation = Registration::get_svd_icp_transformation(source_indices, target_indices);
+    }
+
+    else if (mode == "lm") {
+      cur_transformation = Registration::get_lm_icp_registration(source_indices, target_indices);
+    }
+
+    Registration::source_for_icp_ = Registration::source_for_icp_.transform(cur_transformation);
+    Registration::transformation_ = cur_transformation * Registration::transformation_;
+  }
+
   return;
 }
 
@@ -112,19 +137,22 @@ std::tuple<std::vector<size_t>, std::vector<size_t>, double> Registration::find_
 
   open3d::geometry::KDTreeFlann target_kdtree(target_);
   
+  // For each point in source point cloud
   for (int i = 0; i < source_for_icp_.points_.size(); i++) {
     std::vector<double> cur_distances;
     std::vector<int> cur_indices;
 
+    // Find closest point in target point cloud
     target_kdtree.SearchKNN<Eigen::Vector3d>(source_for_icp_.points_[i], 1, cur_indices, cur_distances);
 
+    // Discard match points pair if distance is bigger than threshold
     if (cur_distances[i] <= threshold) {
       source_indices.push_back(i);
       target_indices.push_back(cur_indices[0]);
     }
   }
 
-  rmse = compute_rmse();
+  rmse = Registration::compute_rmse();
 
   return {source_indices, target_indices, rmse};
 }
@@ -136,7 +164,59 @@ Eigen::Matrix4d Registration::get_svd_icp_transformation(std::vector<size_t> sou
   //Use source_indices and target_indices to extract point to compute the 3x3 matrix to be decomposed.
   //Remember to manage the special reflection case.
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity(4,4);
+  Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity(4, 4);
+
+  Eigen::Matrix3d C = Eigen::Matrix3d::Zero(3, 3);  // Cross-covariance matrix
+  Eigen::Vector3d source_centroid = Eigen::Vector3d::Zero();
+  Eigen::Vector3d target_centroid = Eigen::Vector3d::Zero();
+
+  // Compute centroids
+  for (int i = 0; i < source_indices.size(); i++) {
+    source_centroid += source_for_icp_.points[source_indices[i]];
+    target_centroid += target_.points[target_indices[i]]
+  }
+
+  source_centroid /= source_indices.size();
+  target_centroid /= target_indices.size();
+
+  // Compute cross-covariance matrix C
+  for (int i = 0; i < source_indices.size(); i++) {
+    Eigen::Vector3d cur_source = source_for_icp_[source_indices[i]];
+    Eigen::Vector3d cur_target = target_[target_indices[i]];
+    
+    Eigen::Matrix3d tmp_mul = (cur_source - source_centroid) * (cur_target - target_centroid).transpose();
+    C += tmp_mul;
+  }
+
+  C /= source_indices.size();
+
+  // Compute SVD
+  Eigen::JacobiSVD<Eigen::Matrix3d,  Eigen::DecompositionOptions::ComputeFullU | Eigen::DecompositionOptions::ComputeFullV> svd(C);
+  Eigen::Matrix3d U = svd.matrixU();
+  Eigen::Matrix3d V = svd.matrixV();
+
+  // Manage special reflection case
+  Eigen::Matrix3d S = Eigen::Matrix3d::Identity(3, 3);
+  
+  if (U.determinant() * V.determinant() == -1) {
+    S(2,2) = -1;  // Use operator() to access matrix values since operator[] does not allow for more than one index
+  }
+
+  // Compute rotation matrix and translation vector
+  Eigen::Matrix3d R = V * S * U.transpose();
+  Eigen::Vector3d t = target_centroid - R * source_centroid;
+
+  // Now insert the values in translation matrix
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 3; col++) {
+      transformation(row, col) = R(row, col);
+    }
+  }
+
+  for (int row = 0; row < 3; row++) {
+    transformation(row, 3) = t[row];
+  }
+
   return transformation;
 }
 
@@ -158,6 +238,7 @@ Eigen::Matrix4d Registration::get_lm_icp_registration(std::vector<size_t> source
 
   std::vector<double> transformation_arr(6, 0.0);
   int num_points = source_indices.size();
+
   // For each point....
   for( int i = 0; i < num_points; i++ )
   {
